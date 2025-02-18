6cfd77970231c2e857026668f675e2768eeb3402
swardiantara <swardyantara@gmail.com>
2025-02-19 01:03:42 +0700

    try simple linear layer on-top of the embedding

--- run_multitask.sh
+++ run_multitask.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 datasets=( filtered )
 word_embeds=( drone-severity )
-encoders=( transformer lstm gru )
+encoders=( transformer lstm gru linear )
 weight_class=( uniform )
 losses=( logloss )
 label_schemas=( 101 111 )
@@ -19,11 +19,13 @@ for dataset in "${datasets[@]}"; do
                     for label_schema in "${label_schemas[@]}"; do
                         for pooling in "${poolings[@]}"; do
                             if [ "$encoder" = "none" ]; then
+                                rm -r cache_dir
                                 python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask
                             else
                                 for n_layer in "${n_layers[@]}"; do
                                     if [ "$encoder" = "transformer" ]; then
                                         for n_head in "${n_heads[@]}"; do
+                                            rm -r cache_dir
                                             # for pooling in "${poolings[@]}"; do
                                             python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --n_heads "$n_head" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask
                                             # done
@@ -31,8 +33,10 @@ for dataset in "${datasets[@]}"; do
                                     else
                                         for bidirectional in "${bidirectionals[@]}"; do
                                             if [ "$bidirectional" = true ]; then
+                                                rm -r cache_dir
                                                 python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --bidirectional --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask
                                             else
+                                                rm -r cache_dir
                                                 python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask
                                             fi
                                         done
