347cc1dbe41772de318a1c4674180c5cb3edd94f
swardiantara <swardyantara@gmail.com>
2025-03-06 22:58:36 +0700

    run selected baselines

--- run_baseline.sh
+++ run_baseline.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 datasets=( filtered )
 word_embeds=( bert drone-severity ordinal-severity vector-ordinal )
-encoders=( transformer )
+encoders=( transformer lstm gru none )
 weight_class=( uniform )
 losses=( cross_entropy )
-poolings=( cls )
-bidirectionals=( true )
+poolings=( cls avg max last )
+bidirectionals=( true false )
 n_layers=( 3 )
 n_heads=( 8 )
+seeds=( 14298463 246773155 30288239 82511865 90995999 )
 
 for dataset in "${datasets[@]}"; do
     echo "dataset: "$dataset""
@@ -15,29 +16,31 @@ for dataset in "${datasets[@]}"; do
         for encoder in "${encoders[@]}"; do
             for weight in "${weight_class[@]}"; do
                 for loss in "${losses[@]}"; do
-                    if [ "$encoder" = "none" ]; then
-                        for pooling in "${poolings[@]}"; do
-                            python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline
-                        done
-                    else
-                        for n_layer in "${n_layers[@]}"; do
-                            if [ "$encoder" = "transformer" ]; then
-                                for n_head in "${n_heads[@]}"; do
-                                    for pooling in "${poolings[@]}"; do
-                                        python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --n_heads "$n_head" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline
+                    for seed in "${seeds[@]}"; do
+                        if [ "$encoder" = "none" ]; then
+                            for pooling in "${poolings[@]}"; do
+                                python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
+                            done
+                        else
+                            for n_layer in "${n_layers[@]}"; do
+                                if [ "$encoder" = "transformer" ]; then
+                                    for n_head in "${n_heads[@]}"; do
+                                        for pooling in "${poolings[@]}"; do
+                                            python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --n_heads "$n_head" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
+                                        done
                                     done
-                                done
-                            else
-                                for bidirectional in "${bidirectionals[@]}"; do
-                                    if [ "$bidirectional" = true ]; then
-                                        python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --bidirectional --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline
-                                    else
-                                        python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline
-                                    fi
-                                done
-                            fi
-                        done
-                    fi
+                                else
+                                    for bidirectional in "${bidirectionals[@]}"; do
+                                        if [ "$bidirectional" = true ]; then
+                                            python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --bidirectional --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
+                                        else
+                                            python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
+                                        fi
+                                    done
+                                fi
+                            done
+                        fi
+                    done
                 done
             done
         done
