455af7bdb387ef193e2d9d2db535e95d02fca3a3
swardiantara <swardyantara@gmail.com>
2025-03-07 13:11:17 +0700

    all scenarios of multitask

--- run_multitask.sh
+++ run_multitask.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 datasets=( filtered )
-word_embeds=( bert drone-severity ordinal-severity vector-ordinal )
+word_embeds=( bert drone-severity ordinal-severity vector-max )
 encoders=( transformer lstm gru none )
 weight_class=( uniform )
 losses=( logloss multitask )
-label_schemas=( 0111 )
-decodings=( forward backward argmax )
-poolings=( cls last )
-bidirectionals=( true )
+label_schemas=( 1111 0111 )
+decodings=( forward backward )
+poolings=( cls avg max last )
+bidirectionals=( true false )
 n_layers=( 3 )
 n_heads=( 8 )
+seeds=( 14298463 246773155 30288239 82511865 90995999 )
 
 for dataset in "${datasets[@]}"; do
     echo "dataset: "$dataset""
@@ -19,32 +20,34 @@ for dataset in "${datasets[@]}"; do
                 for loss in "${losses[@]}"; do
                     for label_schema in "${label_schemas[@]}"; do
                         for decoding in "${decodings[@]}"; do
-                            for pooling in "${poolings[@]}"; do
-                                if [ "$encoder" = "none" ]; then
-                                    rm -r cache_dir
-                                    python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask --decoding "$decoding"
-                                else
-                                    for n_layer in "${n_layers[@]}"; do
-                                        if [ "$encoder" = "transformer" ]; then
-                                            for n_head in "${n_heads[@]}"; do
-                                                rm -r cache_dir
-                                                # for pooling in "${poolings[@]}"; do
-                                                python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --n_heads "$n_head" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask --decoding "$decoding"
-                                                # done
-                                            done
-                                        else
-                                            for bidirectional in "${bidirectionals[@]}"; do
-                                                if [ "$bidirectional" = true ]; then
+                            for seed in "${seeds[@]}"; do
+                                for pooling in "${poolings[@]}"; do
+                                    if [ "$encoder" = "none" ]; then
+                                        rm -r cache_dir
+                                        python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask --decoding "$decoding" --seed "$seed"
+                                    else
+                                        for n_layer in "${n_layers[@]}"; do
+                                            if [ "$encoder" = "transformer" ]; then
+                                                for n_head in "${n_heads[@]}"; do
                                                     rm -r cache_dir
-                                                    python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --bidirectional --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask --decoding "$decoding"
-                                                else
-                                                    rm -r cache_dir
-                                                    python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask --decoding "$decoding"
-                                                fi
-                                            done
-                                        fi
-                                    done
-                                fi
+                                                    # for pooling in "${poolings[@]}"; do
+                                                    python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --n_heads "$n_head" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask --decoding "$decoding" --seed "$seed"
+                                                    # done
+                                                done
+                                            else
+                                                for bidirectional in "${bidirectionals[@]}"; do
+                                                    if [ "$bidirectional" = true ]; then
+                                                        rm -r cache_dir
+                                                        python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --bidirectional --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask --decoding "$decoding" --seed "$seed"
+                                                    else
+                                                        rm -r cache_dir
+                                                        python multitask.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --label_schema "$label_schema" --viz_projection --output_dir multitask --decoding "$decoding" --seed "$seed"
+                                                    fi
+                                                done
+                                            fi
+                                        done
+                                    fi
+                                done
                             done
                         done
                     done
