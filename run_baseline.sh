b6809165df88bacee934c42632b62909eb61f4b9
swardiantara <swardyantara@gmail.com>
2025-03-09 15:55:31 +0700

    fix logic error

--- run_baseline.sh
+++ run_baseline.sh
@@ -4,7 +4,7 @@ word_embeds=( bert drone-severity ordinal-severity vector-max )
 encoders=( lstm gru )
 weight_class=( uniform )
 losses=( cross_entropy )
-poolings=( last avg )
+poolings=(last avg)
 bidirectionals=( true false )
 n_layers=( 3 )
 n_heads=( 8 )
@@ -17,29 +17,27 @@ for dataset in "${datasets[@]}"; do
             for weight in "${weight_class[@]}"; do
                 for loss in "${losses[@]}"; do
                     for seed in "${seeds[@]}"; do
-                        if [ "$encoder" = "none" ]; then
-                            for pooling in "${poolings[@]}"; do
+                        for pooling in "${poolings[@]}"; do
+                            if [ "$encoder" = "none" ]; then
                                 python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
-                            done
-                        else
-                            for n_layer in "${n_layers[@]}"; do
-                                if [ "$encoder" = "transformer" ]; then
-                                    for n_head in "${n_heads[@]}"; do
-                                        for pooling in "${poolings[@]}"; do
+                            else
+                                for n_layer in "${n_layers[@]}"; do
+                                    if [ "$encoder" = "transformer" ]; then
+                                        for n_head in "${n_heads[@]}"; do
                                             python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --n_heads "$n_head" --pooling "$pooling" --class_weight "$weight" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
                                         done
-                                    done
-                                else
-                                    for bidirectional in "${bidirectionals[@]}"; do
-                                        if [ "$bidirectional" = true ]; then
-                                            python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --bidirectional --class_weight "$weight" --pooling "$pooling" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
-                                        else
-                                            python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --class_weight "$weight" --pooling "$pooling" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
-                                        fi
-                                    done
-                                fi
-                            done
-                        fi
+                                    else
+                                        for bidirectional in "${bidirectionals[@]}"; do
+                                            if [ "$bidirectional" = true ]; then
+                                                python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --bidirectional --class_weight "$weight" --pooling "$pooling" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
+                                            else
+                                                python baseline.py --dataset "$dataset" --word_embed "$word_embed" --encoder "$encoder" --n_layers "$n_layer" --class_weight "$weight" --pooling "$pooling" --loss "$loss" --save_best_model --viz_projection --output_dir baseline --seed "$seed"
+                                            fi
+                                        done
+                                    fi
+                                done
+                            fi
+                        done
                     done
                 done
             done
